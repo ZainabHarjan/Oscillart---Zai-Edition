1351a147644d60fd7aa2e5a070dd356ead71973f
ZainabHarjan <HarjanZ@catlin.edu>
2025-06-19 18:00:46 +0000

    creating web api elements and creating the ocsilator mode

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const input = document.getElementById('input');
 
+//creating web api elements
+const audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+
+//create Ocsillator mode
+const oscillator = audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = "sine";
+
 function frequency(pitch){
 
 }
